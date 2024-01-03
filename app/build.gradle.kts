9ef1b743fe10f7a2b17b421732b67b554a98a5b1
Manuel Martín Castelao <manuel.martin.castelao@estudiantes.uva.es>
2024-01-03 16:35:34 +0100

    arreglo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.google.code.gson:gson:2.10.1")//necesario para trabajar con json
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
