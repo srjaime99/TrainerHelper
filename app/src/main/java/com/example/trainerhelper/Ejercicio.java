80d07dfd87d260245e24591a912d99313ff3690e
Manuel Martín Castelao <manuel.martin.castelao@estudiantes.uva.es>
2024-01-04 19:05:14 +0100

    IncluirEjercicioActivity.java

--- app/src/main/java/com/example/trainerhelper/Ejercicio.java
+++ app/src/main/java/com/example/trainerhelper/Ejercicio.java
@@ -6,18 +6,25 @@ import org.json.JSONObject;
 public class Ejercicio {
     private String deporte;
     private String nombreEjercicio;
+    private String descripcion;
+    private String materiales;
     private int duracion;
+    private int min;
+    private int max;
 
     // Constructor
-
-    public Ejercicio(String deporte, String nombre, int duracion) {
+    public Ejercicio(String deporte, String nombreEjercicio, String descripcion, String materiales, int duracion, int min, int max) {
         this.deporte = deporte;
-        this.nombreEjercicio = nombre;
+        this.nombreEjercicio = nombreEjercicio;
+        this.descripcion = descripcion;
+        this.materiales = materiales;
         this.duracion = duracion;
+        this.min = min;
+        this.max = max;
     }
 
     public String enTexto(){
-        return ("Deporte: " + this.deporte + ", Nombre: " + this.nombreEjercicio + ", Duracion: " + this.duracion);
+        return ("Deporte: " + this.deporte + ", Nombre: " + this.nombreEjercicio + ", Duracion: " + this.descripcion);
     }
 
     public JSONObject toJson() {
@@ -26,7 +33,7 @@ public class Ejercicio {
         try {
             jsonEjercicio.put("deporte", deporte);
             jsonEjercicio.put("nombre", nombreEjercicio);
-            jsonEjercicio.put("duracion", duracion);
+            jsonEjercicio.put("duracion", descripcion);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -38,9 +45,12 @@ public class Ejercicio {
         try {
             String deporte = jsonObject.getString("deporte");
             String nombre = jsonObject.getString("nombre");
+            String descripcion = jsonObject.getString("descripcion");
+            String materiales = jsonObject.getString("materiales");
             int duracion = jsonObject.getInt("duracion");
-
-            return new Ejercicio(deporte, nombre, duracion);
+            int min = jsonObject.getInt("min");
+            int max = jsonObject.getInt("max");
+            return new Ejercicio(deporte, nombre, descripcion, materiales, duracion, min, max);
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
@@ -48,28 +58,61 @@ public class Ejercicio {
     }
 
     // Getters y setters para todos los atributos
+    //getters
     public String getDeporte() {
         return deporte;
-    }
-
-    public void setDeporte(String deporte) {
-        this.deporte = deporte;
     }
 
     public String getNombreEjercicio() {
         return nombreEjercicio;
     }
 
-    public void setNombreEjercicio(String nombreEjercicio) {
-        this.nombreEjercicio = nombreEjercicio;
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getMateriales() {
+        return materiales;
     }
 
     public int getDuracion() {
         return duracion;
     }
 
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+    //setters
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public void setNombreEjercicio(String nombreEjercicio) {
+        this.nombreEjercicio = nombreEjercicio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setMateriales(String materiales) {
+        this.materiales = materiales;
+    }
+
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
 
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
 }
--- app/src/main/java/com/example/trainerhelper/IncluirEjercicioActivity.java
+++ app/src/main/java/com/example/trainerhelper/IncluirEjercicioActivity.java
@@ -1,56 +1,95 @@
 package com.example.trainerhelper;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class IncluirEjercicioActivity extends AppCompatActivity {
-    //atributos
-    TextView mensajeTextView;
-    EditText mensajeEditText;
-    String[] prueba = {"Natacion", "Patines", "Yoga", "Tae kwondo"};
-    ArrayAdapter<String> adapterItems;
-    AutoCompleteTextView autoCompleteTxt;
-    Spinner spinnerDeportes; //declaramos un spinner que corresponde con el spinner de la vista
+    private Ejercicio ejercicio;
+    private EditText nombreEjercicioEditText;
+    private EditText descripcionEjercicioEditText;
+    private EditText duracionEditText;
+    private EditText minDuracionEditText;
+    private EditText maxDuracionEditText;
+    private EditText materialesEditText;
+    private Spinner deporteSpinner;
+    private TextView resultadoTextView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //este metodo determina lo que hace la actividad cuando se inicia
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anadir_actividad);
+        setContentView(R.layout.activity_anadir_actividad); //interfaz de usuario a ejecutar
 
-        mensajeTextView = findViewById(R.id.DeporTetextView);
-        mensajeTextView.setText("Hola Mundo");
-
-        // Inicializar el Spinner
-        spinnerDeportes = findViewById(R.id.spinner);
-        // Configurar el adaptador del Spinner
-        ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(
+        // Inicializar las vistas
+        nombreEjercicioEditText = findViewById(R.id.nombreEjercicio);
+        descripcionEjercicioEditText = findViewById(R.id.descripcionEjercicio);
+        duracionEditText = findViewById(R.id.duracion);
+        minDuracionEditText = findViewById(R.id.minimo_duracion);
+        maxDuracionEditText = findViewById(R.id.maximo_duracion);
+        materialesEditText = findViewById(R.id.Material);
+        deporteSpinner = findViewById(R.id.spinner);
+        resultadoTextView = findViewById(R.id.resultado);
+        // Cargar los valores del Spinner desde strings.xml
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.deportes, android.R.layout.simple_spinner_item);
-        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerDeportes.setAdapter(adapterSpinner);
-
-        findViewById(R.id.btnVolver).setOnClickListener(new View.OnClickListener() {
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Configurar el adaptador en el Spinner
+        deporteSpinner.setAdapter(adapter);
+        // Configurar el botón "Aceptar"
+        Button aceptarButton = findViewById(R.id.botonSiguiente);
+        aceptarButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {volverAMenu();}
+            public void onClick(View view) {
+                // Lógica para manejar el clic en el botón "Aceptar"
+                crearObjetoDesdeEntradasUsuario();
+                mostrarValoresDelEjercicioCreado();
+            }
         });
+
+        // Otros inicializaciones o configuraciones que puedas necesitar en el método onCreate
     }
 
-    private void volverAMenu() {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
-        finish();
+    // Método para crear un objeto basado en las entradas del usuario
+    private void crearObjetoDesdeEntradasUsuario() {
+        // Obtener los valores de las entradas del usuario
+        String nombreEjercicio = nombreEjercicioEditText.getText().toString();
+        String descripcionEjercicio = descripcionEjercicioEditText.getText().toString();
+        int duracion = Integer.parseInt(duracionEditText.getText().toString());
+        int minDuracion = Integer.parseInt(minDuracionEditText.getText().toString());
+        int maxDuracion = Integer.parseInt(maxDuracionEditText.getText().toString());
+        String materiales = materialesEditText.getText().toString();
+        String deporteSeleccionado = deporteSpinner.getSelectedItem().toString();
+
+        // Crear un nuevo objeto Ejercicio con los valores obtenidos
+        ejercicio = new Ejercicio(deporteSeleccionado, nombreEjercicio, descripcionEjercicio, materiales, duracion, minDuracion, maxDuracion);
     }
-    public void buttonPress(View view){
-        Log.i("Info", "Boton presionado");
-        mensajeEditText = findViewById(R.id.Material);
-        String mensajeString = mensajeEditText.getText().toString();
-        mensajeTextView.setText(mensajeString);
+    private void mostrarValoresDelEjercicioCreado() {
+        // Obtener el último ejercicio creado (suponiendo que lo has almacenado en alguna variable miembro)
+        Ejercicio ejercicioCreado = ejercicio;
+
+        // Comprobar si el ejercicioCreado no es nulo antes de mostrar los valores
+        if (ejercicioCreado != null) {
+            // Mostrar los valores del ejercicio
+            String mensaje = "Valores del ejercicio:\n" +
+                    "Deporte: " + ejercicioCreado.getDeporte() + "\n" +
+                    "Nombre: " + ejercicioCreado.getNombreEjercicio() + "\n" +
+                    "Descripción: " + ejercicioCreado.getDescripcion() + "\n" +
+                    "Materiales: " + ejercicioCreado.getMateriales() + "\n" +
+                    "Duración: " + ejercicioCreado.getDuracion() + "\n" +
+                    "Nº participantes mínimo: " + ejercicioCreado.getMin() + "\n" +
+                    "Nº participantes máximo: " + ejercicioCreado.getMax() + "\n";
+
+            resultadoTextView.setText(mensaje);
+        } else {
+            // Mostrar un mensaje en caso de que no se pueda obtener el ejercicio creado
+            resultadoTextView.setText("No se pudo obtener el ejercicio creado.");
+        }
     }
 }
